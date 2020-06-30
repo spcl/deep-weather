905ca214d96254f10fa6d9234589fb218b4e8255
petergro-hub <65029581+petergro-hub@users.noreply.github.com>
2020-06-30 13:16:38 +0200

    Added more description

--- Uncertainty_Quantification/Preprocessing/preprocessing_parameters.py
+++ Uncertainty_Quantification/Preprocessing/preprocessing_parameters.py
@@ -2,12 +2,12 @@ import os
 
 
 
-WORKERS = 5     # Number of parallel processes to use for preprocessing
-GRIB_PATH = '/mnt/data/weather/ETH_data/ENS10/' # Grib data path
-NPY_PATH = GRIB_PATH+'Preprocessed/'      # Numpy data path
-TFR_PATH = NPY_PATH+'tfr/'                              # TensorFlow Record data path
-CRPS_TEMP_PATH = '/mnt/data/weather/chyao/bias_corr_temp_ens5/'
-CRPS_GEOP_PATH = '/mnt/data/weather/chyao/bias_corr_geo_ens5/'
+WORKERS = 5                                                     # Number of parallel processes to use for preprocessing
+GRIB_PATH = '/mnt/data/weather/ETH_data/ENS10/'                 # Grib data path
+NPY_PATH = GRIB_PATH+'Preprocessed/'                            # Numpy data path
+TFR_PATH = NPY_PATH+'tfr/'                                      # TensorFlow Record data path
+CRPS_TEMP_PATH = '/mnt/data/weather/chyao/bias_corr_temp_ens5/' # Path to bias corrected predictions and ground truth for temperature
+CRPS_GEOP_PATH = '/mnt/data/weather/chyao/bias_corr_geo_ens5/'  # Path to bias corrected predictions and ground truth for temperature
 if not os.path.exists(NPY_PATH):
     os.makedirs(NPY_PATH)
 if not os.path.exists(TFR_PATH):
